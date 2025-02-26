dbadfdb1714785cbfc417d5fb7c79174da46e31a
Ava Tuttle <Travis.Tuttle@sdgku.edu>
2025-02-25 17:57:20 -0700

    getting there

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.about_view, name="about"),
+    path("contact/", views.contact_view, name="contact"),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from .forms import ContactForm
 # Create your views here.
 def my_first_view(request):
     return render(request, "base.html")
 
 def about_view(request):
-    return render(request, "about_me.html")
+    return render(request, "pages/about_me.html")
+
+def contact_view(request):
+    form = ContactForm()
+    return render(request, "pages/contact.html", {"form": form})
